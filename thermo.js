b593ed52c935e84f46a510877709c08f60c11019
Panos L <panos.lefteratos@gmail.com>
2016-12-23 22:53:18 +0200

    Updated to handle negative value temperatures

--- thermo.js
+++ thermo.js
@@ -4,6 +4,9 @@
 /* Press button once. If temperature is 25C  */
 /* then the RED LED will flash twice and the */
 /* GREEN LED will flash 5 times.             */
+/* If temperature is -25C the BLUE LED will  */
+/* flash once, the RED LED will flash twice  */
+/* and the GREEN LED will flash 5 times.     */
 /*********************************************/
 
 function flashRED() {
@@ -28,27 +31,38 @@ function flashBLUE() {
 }
 
 var digits = [0, 0];
+var negative = false;
 setInterval(function() {
   if(digitalRead(BTN)) {
     temp = E.getTemperature();
-    console.log('temp: ' + temp);
+    console.log("temperature: " + temp);
+    /* check if negative temp exists */
+    negative = temp<0 || false;
+    temp = Math.abs(temp);
     digits = parseInt(temp).toString(10).split("").map(Number);
+    /* always make sure the `digits` array is of length 2 */
+    if(digits.length===1) {
+      digits.unshift(0);
+    }
     console.log(digits);
   }
-    
-  if(digits[0]>0) {
-    console.log("flashRED");
-    flashRED();
+  
+  if(negative) {
+    console.log("flashBLUE");
+    flashBLUE();
+    negative = false;
+  } else {
+    if(digits[0]>0) {
+      console.log("flashRED");
+      flashRED();
+    }
+    if(digits[0] <= 0) {
+     if(digits[1]>0) {
+       console.log("flashGREEN");
+       flashGREEN();
+       digits[1]--;
+     }
+    }
+    digits[0]--;
   }
-  
-  if(digits[0] <= 0) {
-   if(digits[1]>0) {
-     console.log("flashGREEN");
-     flashGREEN();
-     digits[1]--;
-   }
-  }
-  
-  digits[0]--;
-  
 }, 800);
